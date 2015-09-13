5235e1593ba9b3bb00ab529cb5299233d2c483c1
Himanshu Gupta <g.himanshu@gmail.com>
2015-09-12 22:40:01 -0500

    fixing minor error in DruidUtils

--- druid-pig/src/main/java/com/yahoo/druid/pig/udfs/DruidUtils.java
+++ druid-pig/src/main/java/com/yahoo/druid/pig/udfs/DruidUtils.java
@@ -17,6 +17,6 @@ public class DruidUtils
   public static boolean isComplex(String type)
   {
     return type.equals("complex") ||
-           !(type.equals("float") || !type.equals("long") || type.equals("string") || type.equals("simple"));
+           !(type.equals("float") || type.equals("long") || type.equals("string") || type.equals("simple"));
   }
 }
