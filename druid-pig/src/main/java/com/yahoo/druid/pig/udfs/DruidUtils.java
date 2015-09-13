e0fd6a86a41af8b232dbf903a361150600645603
Himanshu Gupta <g.himanshu@gmail.com>
2015-09-12 22:18:27 -0500

    adding DruidUtils for reuse of some common code

---
+++ druid-pig/src/main/java/com/yahoo/druid/pig/udfs/DruidUtils.java
@@ -0,0 +1,11 @@
+package com.yahoo.druid.pig.udfs;
+
+/**
+ */
+public class DruidUtils
+{
+  public static boolean isComplex(String type)
+  {
+    return !type.equals("float") && !type.equals("long") && !type.equals("string") && !type.equals("simple");
+  }
+}
